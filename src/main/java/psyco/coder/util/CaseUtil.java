f4d3d04a9c92fc63e6c265a21e383b87902b114b
peng <dianxiangan@aliyun.com>
2015-10-16 16:21:15 +0800

    修复ProcessingInstruction & user search

--- src/main/java/psyco/coder/util/CaseUtil.java
+++ src/main/java/psyco/coder/util/CaseUtil.java
@@ -14,4 +14,21 @@ public class CaseUtil {
     public static String getter(String s) {
         return "get" + CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, s);
     }
+
+    public CaseFormat detectFormat(String s) {
+        if (s.contains("_")) {
+            return isLower(s.charAt(0)) ? CaseFormat.LOWER_UNDERSCORE : CaseFormat.LOWER_UNDERSCORE;
+        }
+        if (s.contains("-"))
+            return CaseFormat.LOWER_HYPHEN;
+        return isLower(s.charAt(0)) ? CaseFormat.LOWER_CAMEL : CaseFormat.UPPER_CAMEL;
+    }
+
+    public static boolean isLower(char a) {
+        return !(a >= 'A' && a <= 'Z');
+    }
+
+    public static boolean isUpper(char a) {
+        return !(a >= 'a' && a <= 'z');
+    }
 }
